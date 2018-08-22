a4228502ad8372a73a3d5b2d50977cce67d53cf4
ls739@cam.ac.uk <25596077+Osmu@users.noreply.github.com>
2018-08-22 01:28:53 +0100

    Temporary addition of server files

---
+++ Test/lidgren/client.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.IO;
+using Lidgren.Network;
+
+namespace lidgren_client {
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class GameClient : Game {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+
+        string consoleOutput;
+
+        bool connected;
+
+        private Random random; //
+
+        static NetClient Client;
+
+        enum PacketTypes {
+            JOIN,
+            CHOICE,
+            DEBUG
+        }
+
+        public GameClient() {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+
+        protected override void Initialize() {
+
+            random = new Random();
+
+            string path = Path.Combine(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory), @"ip.txt");
+            StreamReader sr = new StreamReader(path);
+            string ip = sr.ReadLine();
+
+            NetPeerConfiguration Config = new NetPeerConfiguration("alpaka");
+            Client = new NetClient(Config);
+
+            // Create hail message
+            NetOutgoingMessage hailmsg = Client.CreateMessage();
+
+            Client.Start();
+
+            hailmsg.Write((byte)PacketTypes.JOIN);
+            hailmsg.Write((byte)0); //Whether spectating or participating
+            hailmsg.Write("Username"); //Name of user
+
+            Client.Connect(ip, 20000, hailmsg);
+
+            consoleOutput += "Client Started...\n";
+
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent() {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("File");
+        }
+
+
+        protected override void UnloadContent() {
+            // TODO: Unload any non ContentManager content here
+        }
+
+
+        protected override void Update(GameTime gameTime) {
+            CheckServerMessages();
+            if (connected && Keyboard.GetState().IsKeyDown(Keys.C)) {
+                GetInput();
+            }
+            base.Update(gameTime);
+        }
+
+        private void GetInput() {
+            byte a = (byte)random.Next(0, 4);
+            byte b = (byte)random.Next(0, 6);
+            byte c = (byte)random.Next(0, 8);
+
+            NetOutgoingMessage outmsg = Client.CreateMessage();
+
+            outmsg.Write((byte)PacketTypes.CHOICE);
+            outmsg.Write(a);
+            outmsg.Write(b);
+            outmsg.Write(c);
+            Client.SendMessage(outmsg, NetDeliveryMethod.ReliableOrdered);
+            string s = "Choice Sent... (" + a + " " + b + " " + c + ")";
+            Console.WriteLine(s);
+            consoleOutput += s + "\n";
+        }
+
+        private void CheckServerMessages() {
+            NetIncomingMessage msg;
+
+            // If new messages arrived
+            if ((msg = Client.ReadMessage()) != null) {
+                string s = "";
+
+                switch (msg.MessageType) {
+                    case NetIncomingMessageType.Data:
+                    PacketTypes type = (PacketTypes)msg.ReadByte();
+                    switch (type) {
+                        case PacketTypes.JOIN:
+                        connected = true;
+                        s = msg.MessageType + ":[" + type + "] " + msg.ReadString();
+                        break;
+                        case PacketTypes.CHOICE:
+                        s = msg.MessageType + ":[" + type + "] " + msg.ReadByte() + " " + msg.ReadByte() + " " + msg.ReadByte();
+                        break;
+                    }
+
+                    break;
+                    case NetIncomingMessageType.StatusChanged:
+                    s = msg.MessageType + ":[" + (NetConnectionStatus)msg.ReadByte() + "] " + msg.ReadString();
+                    break;
+                    default:
+                    s = msg.MessageType + ": " + msg.ReadString();
+                    break;
+                }
+                Console.WriteLine(s);
+                consoleOutput += s + "\n";
+            }
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, consoleOutput, new Vector2(0, 0), Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Test/lidgren/server.cs
@@ -0,0 +1,146 @@
+﻿using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lidgren_server {
+
+    class Program {
+
+        static NetServer Server;
+        static NetPeerConfiguration Config;
+        static NetConnection Player1Connection;
+        static NetConnection Player2Connection;
+
+        static List<NetConnection> WaitingPlayerConnections;
+        static List<NetConnection> WaitingSpectatorConnections;
+
+        static void Main(string[] args) {
+
+            Config = new NetPeerConfiguration("alpaka");
+            Config.Port = 20000;
+            Config.MaximumConnections = 200;
+
+            Config.EnableMessageType(NetIncomingMessageType.ConnectionApproval);
+
+            Server = new NetServer(Config);
+            Server.Start();
+            Console.WriteLine("Server Started...");
+
+            WaitingPlayerConnections = new List<NetConnection>();
+            WaitingSpectatorConnections = new List<NetConnection>();
+            NetIncomingMessage msg;
+
+            while (true) {
+                if ((msg = Server.ReadMessage()) != null) {
+
+                    switch (msg.MessageType) {
+                        case NetIncomingMessageType.ConnectionApproval:
+
+                        if (msg.ReadByte() == (byte)PacketTypes.JOIN) {
+                            Console.WriteLine("Incoming JOIN");
+                            byte b = msg.ReadByte();
+                            if (b == 0) {
+                                if (Player1Connection == null || Player2Connection == null) {
+                                    msg.SenderConnection.Approve();
+                                    WaitingPlayerConnections.Add(msg.SenderConnection);
+                                    Console.WriteLine("Pending new connection to: " + msg.ReadString());
+
+                                } else {
+                                    msg.SenderConnection.Deny();
+                                    Console.WriteLine("Denied connection to: " + msg.ReadString());
+                                }
+                            } else if (b == 1) {
+                                msg.SenderConnection.Approve();
+                                WaitingSpectatorConnections.Add(msg.SenderConnection);
+                                Console.WriteLine("Pending new connection to: " + msg.ReadString());
+
+                            } else {
+                                msg.SenderConnection.Deny();
+                                Console.WriteLine("Denied connection to: " + msg.ReadString());
+                            }
+                        }
+
+                        break;
+
+                        case NetIncomingMessageType.Data:
+                        if (msg.ReadByte() == (byte)PacketTypes.CHOICE) {
+
+                            NetOutgoingMessage outmsg = Server.CreateMessage();
+                            outmsg.Write((byte)PacketTypes.CHOICE);
+                            if (msg.SenderConnection == Player1Connection) {
+                                Console.WriteLine("Player1 Choice Recieved - Forwarding to Player2...");
+                                outmsg.Write(msg.ReadByte());
+                                outmsg.Write(msg.ReadByte());
+                                outmsg.Write(msg.ReadByte());
+                                Server.SendMessage(outmsg, Player2Connection, NetDeliveryMethod.ReliableOrdered);
+                            } else if (msg.SenderConnection == Player2Connection) {
+                                Console.WriteLine("Player2 Choice Recieved - Forwarding to Player1...");
+                                outmsg.Write(msg.ReadByte());
+                                outmsg.Write(msg.ReadByte());
+                                outmsg.Write(msg.ReadByte());
+                                Server.SendMessage(outmsg, Player1Connection, NetDeliveryMethod.ReliableOrdered);
+
+                            }
+                        }
+                        break;
+
+                        case NetIncomingMessageType.StatusChanged:
+
+                        Console.WriteLine(msg.SenderConnection.ToString() + " status changed. " + msg.SenderConnection.Status);
+                        switch (msg.SenderConnection.Status) {
+                            case NetConnectionStatus.Connected:
+                            NetOutgoingMessage outmsg = Server.CreateMessage();
+
+                            if (WaitingPlayerConnections.Contains(msg.SenderConnection) && (Player1Connection == null || Player2Connection == null)) {
+                                outmsg.Write((byte)PacketTypes.JOIN);
+                                if (Player1Connection == null) {
+                                    Player1Connection = msg.SenderConnection;
+                                    Console.WriteLine("Player1 Joined");
+                                    outmsg.Write("Sucessfully Joined as Player1");
+                                } else {
+                                    Player2Connection = msg.SenderConnection;
+                                    Console.WriteLine("Player2 Joined");
+                                    outmsg.Write("Sucessfully Joined as Player2");
+                                }
+                            } else if (WaitingSpectatorConnections.Contains(msg.SenderConnection)) {
+                                outmsg.Write((byte)PacketTypes.JOIN);
+                                Console.WriteLine("New Spectator Joined");
+                                outmsg.Write("Sucessfully Joined as Spectator");
+                            } else {
+                                Console.WriteLine("Too Many Connections - Could not Connect");
+                                msg.SenderConnection.Deny();
+                            }
+                            Server.SendMessage(outmsg, msg.SenderConnection, NetDeliveryMethod.ReliableOrdered);
+
+                            break;
+                            case NetConnectionStatus.InitiatedConnect:
+                            break;
+                            default:
+                            break;
+                        }
+
+                        //   if (inc.SenderConnection.Status == NetConnectionStatus.Disconnected || inc.SenderConnection.Status == NetConnectionStatus.Disconnecting)
+                        // Find disconnected character and remove it
+
+                        break;
+                        default:
+                        Console.WriteLine("Not Important Message");
+                        break;
+                    }
+                }
+
+                // While loops run as fast as your computer lets. While(true) can lock your computer up. Even 1ms sleep, lets other programs have piece of your CPU time
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+    }
+
+    enum PacketTypes {
+        JOIN,
+        CHOICE,
+        DEBUG
+    }
+}
